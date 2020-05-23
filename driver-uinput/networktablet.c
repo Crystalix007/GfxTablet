0fb9933d5c2fb3a48cbf43dbc9a57ae0f4e9192b
Michael Kuc <michaelkuc6@gmail.com>
2020-05-23 10:50:23 +0100

    Reformat code to consistent standard.

--- driver-uinput/networktablet.c
+++ driver-uinput/networktablet.c
@@ -1,29 +1,28 @@
-
+#include "protocol.h"
+#include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <X11/X.h>
-#include <cairo.h>
+#include <arpa/inet.h>
 #include <cairo-xlib.h>
+#include <cairo.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <linux/input.h>
+#include <linux/uinput.h>
 #include <pthread.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <limits.h>
-#include <arpa/inet.h>
-#include <linux/input.h>
-#include <linux/uinput.h>
-#include <stdint.h>
-#include "protocol.h"
 
-#define die(str, args...) { \
-	perror(str); \
-	exit(EXIT_FAILURE); \
-}
-
+#define die(str, args...)                                                                          \
+	{                                                                                                \
+		perror(str);                                                                                   \
+		exit(EXIT_FAILURE);                                                                            \
+	}
 
 int udp_socket, sending;
 
@@ -35,9 +34,7 @@ typedef struct sending_t {
 	int slen;
 } sending_t;
 
-
-void init_device(int fd)
-{
+void init_device(int fd) {
 	struct uinput_user_dev uidev;
 
 	// enable synchronization
@@ -69,7 +66,7 @@ void init_device(int fd)
 	memset(&uidev, 0, sizeof(uidev));
 	snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "Network Tablet");
 	uidev.id.bustype = BUS_VIRTUAL;
-	uidev.id.vendor  = 0x1;
+	uidev.id.vendor = 0x1;
 	uidev.id.product = 0x1;
 	uidev.id.version = 1;
 	uidev.absmin[ABS_X] = 0;
@@ -85,8 +82,7 @@ void init_device(int fd)
 		die("error: ioctl");
 }
 
-int prepare_socket()
-{
+int prepare_socket() {
 	int s;
 	sockaddr_in addr;
 
@@ -98,14 +94,13 @@ int prepare_socket()
 	addr.sin_port = htons(GFXTABLET_PORT);
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+	if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) == -1)
 		die("error: prepare_socket()");
 
 	return s;
 }
 
-void send_event(int device, int type, int code, int value)
-{
+void send_event(int device, int type, int code, int value) {
 	struct input_event ev;
 	ev.type = type;
 	ev.code = code;
@@ -118,18 +113,18 @@ void quit(int signal) {
 	close(udp_socket);
 }
 
-void msleep(int ms){
-	struct timespec req = {0};
+void msleep(int ms) {
+	struct timespec req = { 0 };
 	req.tv_sec = 0;
 	req.tv_nsec = ms * 1000000L;
-	nanosleep(&req, (struct timespec *)NULL);
+	nanosleep(&req, (struct timespec*)NULL);
 }
 
-void *send_current_screen(void *arg){
-	sending_t *args = (sending_t*) arg;
+void* send_current_screen(void* arg) {
+	sending_t* args = (sending_t*)arg;
 	sockaddr_in from = args->from;
 	int slen = args->slen;
-	Display *disp = XOpenDisplay(":0");
+	Display* disp = XOpenDisplay(":0");
 
 	if (sending || !disp) {
 		return NULL;
@@ -137,52 +132,51 @@ void *send_current_screen(void *arg){
 	printf("\nsend_thread\n");
 
 	msleep(300);
-	sending=1;
+	sending = 1;
 	Window root;
-	cairo_surface_t *surface;
+	cairo_surface_t* surface;
 	int scr;
 	scr = DefaultScreen(disp);
 	root = DefaultRootWindow(disp);
 	/* get the root surface on given display */
-	surface = cairo_xlib_surface_create(disp, root, DefaultVisual(disp, scr),
-													DisplayWidth(disp, scr),
-													DisplayHeight(disp, scr));
+	surface = cairo_xlib_surface_create(disp, root, DefaultVisual(disp, scr), DisplayWidth(disp, scr),
+	                                    DisplayHeight(disp, scr));
 	/* right now, the tool only outputs PNG images */
-	cairo_surface_write_to_png( surface, "test.png" );
+	cairo_surface_write_to_png(surface, "test.png");
 	/* free the memory*/
 	cairo_surface_destroy(surface);
 
-	FILE *istream;
-	if ( (istream = fopen("test.png", "r" ) ) == NULL ){
+	FILE* istream;
+	if ((istream = fopen("test.png", "r")) == NULL) {
 		die("file non-existant!");
 	}
 	from.sin_port = htons(GFXTABLET_PORT);
-	int max=60000;
-	char buff[max+30];
-	int n=1;
-	while(fread(buff,sizeof(char),max,istream) != 0){
+	int max = 60000;
+	char buff[max + 30];
+	int n = 1;
+	while (fread(buff, sizeof(char), max, istream) != 0) {
 		printf("Send packet to %s:%d\n", inet_ntoa(from.sin_addr), ntohs(from.sin_port));
-		buff[max +29] = n;
-		if (sendto(udp_socket, buff, sizeof(buff), 0, (struct sockaddr*) &from, slen) == -1){
+		buff[max + 29] = n;
+		if (sendto(udp_socket, buff, sizeof(buff), 0, (struct sockaddr*)&from, slen) == -1) {
 			die("sendto()");
 		}
 		n++;
-		for (int r = 0; r <= max ; r++){
+		for (int r = 0; r <= max; r++) {
 			buff[r] = 0;
 		}
 	}
-	fclose (istream );
+	fclose(istream);
 	char buf[1];
-	buf[0] = n-1;
-	if (sendto(udp_socket, buf, strlen(buff)+1, 0, (struct sockaddr*) &from, slen) == -1){
+	buf[0] = n - 1;
+	if (sendto(udp_socket, buf, strlen(buff) + 1, 0, (struct sockaddr*)&from, slen) == -1) {
 		die("sendto()");
 	}
-	sending=0;
+	sending = 0;
 }
 
-int main(void){
+int main(void) {
 	int device;
-	sending=0;
+	sending = 0;
 	event_packet ev_pkt;
 	sending_t sock_t;
 	pthread_t ev_retreive_t, screen_send_t;
@@ -194,13 +188,16 @@ int main(void){
 	udp_socket = prepare_socket();
 
 	printf("GfxTablet driver (protocol version %u) is ready and listening on 0.0.0.0:%u (UDP)\n"
-		"Hint: Make sure that this port is not blocked by your firewall.\n", PROTOCOL_VERSION, GFXTABLET_PORT);
+	       "Hint: Make sure that this port is not blocked by your firewall.\n",
+	       PROTOCOL_VERSION, GFXTABLET_PORT);
 
 	signal(SIGINT, quit);
 	signal(SIGTERM, quit);
 
-	while (recvfrom(udp_socket, &ev_pkt, sizeof(event_packet), 0, (struct sockaddr *) &sock_t.from, &sock_t.slen) >= 9) {		// every packet has at least 9 bytes
-		printf("."); fflush(0);
+	while (recvfrom(udp_socket, &ev_pkt, sizeof(event_packet), 0, (struct sockaddr*)&sock_t.from,
+	                &sock_t.slen) >= 9) { // every packet has at least 9 bytes
+		printf(".");
+		fflush(0);
 
 		if (memcmp(ev_pkt.signature, "GfxTablet", 9) != 0) {
 			fprintf(stderr, "\nGot unknown packet on port %i, ignoring\n", GFXTABLET_PORT);
@@ -208,8 +205,10 @@ int main(void){
 		}
 		ev_pkt.version = ntohs(ev_pkt.version);
 		if (ev_pkt.version != PROTOCOL_VERSION) {
-			fprintf(stderr, "\nGfxTablet app speaks protocol version %i but driver speaks version %i, please update\n",
-				ev_pkt.version, PROTOCOL_VERSION);
+			fprintf(stderr,
+			        "\nGfxTablet app speaks protocol version %i but driver speaks version %i, "
+			        "please update\n",
+			        ev_pkt.version, PROTOCOL_VERSION);
 			break;
 		}
 
@@ -245,7 +244,7 @@ int main(void){
 		}
 
 		if (ev_pkt.pressure == 0 && memcmp(inet_ntoa(sock_t.from.sin_addr), "0.0.0.0", 7) != 0) {
-			if(pthread_create(&screen_send_t, NULL, send_current_screen, &sock_t)) {
+			if (pthread_create(&screen_send_t, NULL, send_current_screen, &sock_t)) {
 				fprintf(stderr, "Error creating thread\n");
 				return 1;
 			}
